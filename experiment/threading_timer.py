6eb45cd6aad28b43bcab88277d9dec0203af56da
Tu Dang <huynh.tu.dang@usi.ch>
2016-05-30 13:37:13 +0200

    python threading timer example

---
+++ experiment/threading_timer.py
@@ -0,0 +1,12 @@
+import threading
+
+def hello(*args, **kwargs):
+    for x in args:
+        print x
+
+    for k,v in kwargs.items():
+        print k, v
+
+t = threading.Timer(1, hello, ['1', '2'], {'arg1': 1, 'arg2': 2})
+
+t.start()
